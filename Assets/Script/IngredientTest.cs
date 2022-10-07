bdae4743a3bbc7ad212e1a830a5a2bbfc59a06eb
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-07 19:50:58 +0200

    Add test for comparing ingredient values

---
+++ Assets/Script/IngredientTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script
+{
+    public class IngredientTest : MonoBehaviour
+    {
+
+        private void Awake()
+        {
+            // get all ingredients from Resources
+            Ingredient[] ingredients = Resources.LoadAll<Ingredient>("Ingredients");
+            
+            // order ingredients by property
+            Array.Sort(ingredients, (x, y) => y.Sweetness.CompareTo(x.Sweetness));
+            
+            // print ingredients
+            string output = "";
+            foreach (Ingredient ingredient in ingredients)
+            {
+                output += ingredient.Sweetness + " " + ingredient.name + "\n";
+            }
+            
+            Debug.Log(output);
+        }
+    }
+}
