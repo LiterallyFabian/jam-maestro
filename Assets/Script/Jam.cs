81698244f3e208e4e80fc5993e000c719278f963
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-07 20:43:25 +0200

    Add test jams

---
+++ Assets/Script/Jam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Script
+{
+    [CreateAssetMenu(fileName = "New Jam", menuName = "Jam/Jam")]
+    public class Jam : ScriptableObject
+    {
+        public List<Ingredient> Ingredients = new List<Ingredient>();
+        
+        public float SugarAmount;
+        
+        public JamScore Score => new JamScore(this);
+
+        public string GetIngredients()
+        {
+            IEnumerable<IGrouping<string, Ingredient>> groups = Ingredients.GroupBy(f => f.Name);
+            return string.Join(", ", groups.Select(f => f.Count() > 1 ? $"{f.Key} x{f.Count()}" : f.Key));
+        }
+    }
+}
