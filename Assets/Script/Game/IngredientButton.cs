4b411ee18aaf4752d92d569c098e18d94e4f2b26
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-08 11:31:16 +0200

    Add simple button

---
+++ Assets/Script/Game/IngredientButton.cs
@@ -0,0 +1,39 @@
+﻿using JamMeistro.Jams;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace JamMeistro.Game
+{
+    [RequireComponent(typeof(Button))]
+    public class IngredientButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Button _button;
+        
+        [Tooltip("The text should be hidden unless the user hovers this button")]
+        private Text _text;
+        [SerializeField] private Ingredient _ingredient;
+        
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _text = GetComponentInChildren<Text>();
+
+            _text.text = _ingredient.Name;
+            _text.enabled = false;
+            
+            if (_ingredient == null)
+                Debug.LogError($"Ingredient is missing on {name}");
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _text.enabled = true;
+        }
+        
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _text.enabled = false;
+        }
+    }
+}
