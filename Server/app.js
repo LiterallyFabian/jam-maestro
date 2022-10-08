4e05e376fbc8c6ec7120fb8c4e3d9e34bd496312
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-08 22:32:01 +0200

    Remove modules

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let path = require('path');
-let sassMiddleware = require('node-sass-middleware');
 const http = require('http');
 const mysql2 = require("mysql2");
 require('dotenv').config();
@@ -31,12 +30,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
