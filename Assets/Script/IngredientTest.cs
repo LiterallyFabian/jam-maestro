e45ebe0b15bfa6e87158e6d4cf0b237b07e92077
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-08 11:08:31 +0200

    Add JSON report for ingredients

--- Assets/Script/IngredientTest.cs
+++ Assets/Script/IngredientTest.cs
@@ -1,30 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Script
 {
     public class IngredientTest : MonoBehaviour
     {
+        private Ingredient[] _ingredients;
         private void Awake()
         {
+             _ingredients = Resources.LoadAll<Ingredient>("Ingredients");
         }
 
         private void PrintValues()
         {
-            // get all ingredients from Resources
-            Ingredient[] ingredients = Resources.LoadAll<Ingredient>("Ingredients");
-            
             // order ingredients by property
-            Array.Sort(ingredients, (x, y) => y.Sweetness.CompareTo(x.Sweetness));
+            Array.Sort(_ingredients, (x, y) => y.Sweetness.CompareTo(x.Sweetness));
             
             // print ingredients
             string output = "";
-            foreach (Ingredient ingredient in ingredients)
+            foreach (Ingredient ingredient in _ingredients)
             {
                 output += ingredient.Sweetness + " " + ingredient.name + "\n";
             }
             
             Debug.Log(output);
+        }
+
+        private void GenerateIngredientJson()
+        {
+            string output = JsonConvert.SerializeObject(_ingredients, Formatting.Indented);
+            
+            string path = Application.temporaryCachePath + "/ingredients.json";
+            File.WriteAllText(path, output);
+            Process.Start("notepad.exe", path);
         }
     }
 }
