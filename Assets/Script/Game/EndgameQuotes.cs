d463ed703321c0cc0773cf5ec25e3036819b6f2a
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-09 12:14:49 +0200

    Add quote template

---
+++ Assets/Script/Game/EndgameQuotes.cs
@@ -0,0 +1,16 @@
+﻿namespace JamMeistro.Game
+{
+    public class EndgameQuotes
+    {
+        public static string[] Heavenly = {"Quote"};
+        public static string[] Good = {"Quote"};
+        public static string[] Neutral = {"Quote"};
+        public static string[] Bad = {"Quote"};
+        public static string[] Spicy = {"Quote"};
+        public static string[] Sour = {"Quote"};
+        public static string[] Horrible = {
+            "Ah this is bad", 
+            "This is horrible", "This is the worst",
+        };
+    }
+}
