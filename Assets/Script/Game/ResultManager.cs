d9907f61522058ce554a649d09404f9bfed5b0bd
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-10-09 09:50:38 +0200

    Add prompt to login

--- Assets/Script/Game/ResultManager.cs
+++ Assets/Script/Game/ResultManager.cs
@@ -2,27 +2,42 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace JamMeistro.Game
 {
     public class ResultManager : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _scoreText;
+        [SerializeField] private GameObject _buttons;
+        [SerializeField] private Button _leaderboardButton;
+
+        private void Start()
+        {
+            if (PlayerPrefs.GetString("User", "").Length == 0)
+            {
+                _leaderboardButton.GetComponentInChildren<Text>().text = "Join leaderboard";
+                _leaderboardButton.onClick = new Button.ButtonClickedEvent();
+                _leaderboardButton.onClick.AddListener(() => SceneManager.LoadScene("Main"));
+            }
+        }
 
         public void ShowResult(Jam j, string pos)
         {
             gameObject.SetActive(true);
             _scoreText.text = j.Score.ToResultText(pos);
+            _buttons.SetActive(true);
         }
         
         public void ClearResult()
         {
             _scoreText.text = "Tasting...";
+            _buttons.SetActive(false);
         }
 
         public void NewGame()
         {
-            SceneManager.LoadScene("Main");
+            SceneManager.LoadScene("Game");
         }
 
         public void OpenLeaderboard()
