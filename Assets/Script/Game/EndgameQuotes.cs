dfbc38766a3e0e4d7d9657545a23881c9a05b483
noroalia <noroalias@gmail.com>
2022-10-09 13:24:58 +0200

    Update EndgameQuotes.cs

--- Assets/Script/Game/EndgameQuotes.cs
+++ Assets/Script/Game/EndgameQuotes.cs
@@ -2,15 +2,19 @@
 {
     public class EndgameQuotes
     {
-        public static string[] Heavenly = {"Quote"};
-        public static string[] Good = {"Quote"};
-        public static string[] Neutral = {"Quote"};
-        public static string[] Bad = {"Quote"};
-        public static string[] Spicy = {"Quote"};
-        public static string[] Sour = {"Quote"};
+        public static string[] Heavenly = {"This is incredible!", "Absolutely beautiful!", "Perfect composition and balancing!", "Incredible!!!"};
+        public static string[] Good = {"Pretty good!", "A decent jam!", "It's good!", "It's nice, but can be improved!", 
+            "I don't think it will wow any judges, but it's a good jam either way."};
+        public static string[] Neutral = {"Yeah, it's fine, but not much more than that.", 
+            "Not what I would call an award-winning jam.", "Eh.", "Average at best.", "Good try!"};
+        public static string[] Bad = {"Eugh, maybe not.", "Could've been worse but... Still not good.", "Nice try! Maybe.", 
+            "Sometimes throwing things at the wall and seeing what sticks isn't a good strategy."};
+        public static string[] Spicy = {"FAR TOO HOT!", "Did you just put chili in this?!", "This isn't jam, this is hot sauce!"};
+        public static string[] Sour = {"AGH! TOO SOUR!", "Next time, don't put in that many lemons!", "I feel like I'm having a heart attack!", };
         public static string[] Horrible = {
-            "Ah this is bad", 
-            "This is horrible", "This is the worst",
+            "Ugh, this is disgusting!", 
+            "What did you put in this?!", "Just... why?" ,"Are you doing this on purpose?", 
+            "Did you seriously think this was going to be good?", "Are you trying to kill me?"
         };
     }
 }
